bdb6c05af81cc7b076036ba81dbcbf740658432a
Fernando Godinho <fppgodinho@gmail.com>
2015-11-16 17:08:32 +0000

    Create Product view has been implemented

--- src/js/directive/menu.js
+++ src/js/directive/menu.js
@@ -1,9 +1,33 @@
 var angular     = require('angular');
 
-angular.module('spaExercise').directive('menu', function (){
+angular.module('spaExercise').directive('menu', function ()                                                             {
     return {
         restrict: 'E',
         replace: 'true',
-        template: require('./../../html/template/menu.jade')
+        template: require('./../../html/template/menu.jade'),
+        controller: ['$scope', '$uibModal', function($scope, $uibModal)                                                 {
+
+            $scope.open = function ()                                                                                   {
+                var modalInstance = $uibModal.open({
+                    animation:  true,
+                    template:   require('./../../html/template/menu-new-product.jade')(),
+                    controller: ['$scope', 'modelProducts', function($scope, modelProducts)                             {
+                        $scope.name         = '';
+                        $scope.description  = '';
+
+                        $scope.ok           = function ()                                                               {
+                            modelProducts.create({name: $scope.name, description: $scope.description}, function (err, id){
+                                console.log(err, id);
+                                modalInstance.close();
+                            });
+                        };
+
+                        $scope.cancel       = function ()                                                               {
+                            modalInstance.dismiss('cancel');
+                        };
+                    }]
+                });
+            };
+        }]
     }
 });

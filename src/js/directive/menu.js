e726815cab80b15a77ffe716efb584d9c4404938
Fernando Godinho <fppgodinho@gmail.com>
2015-11-16 17:43:52 +0000

    Optimized the create product view

--- src/js/directive/menu.js
+++ src/js/directive/menu.js
@@ -2,32 +2,33 @@ var angular     = require('angular');
 
 angular.module('spaExercise').directive('menu', function ()                                                             {
     return {
-        restrict: 'E',
-        replace: 'true',
-        template: require('./../../html/template/menu.jade'),
+        restrict:   'E',
+        replace:    'true',
+        template:   require('./../../html/template/menu.jade'),
         controller: ['$scope', '$uibModal', function($scope, $uibModal)                                                 {
-
             $scope.open = function ()                                                                                   {
-                var modalInstance = $uibModal.open({
+                $uibModal.open({
                     animation:  true,
                     template:   require('./../../html/template/menu-new-product.jade')(),
-                    controller: ['$scope', 'modelProducts', function($scope, modelProducts)                             {
-                        $scope.name         = '';
-                        $scope.description  = '';
-
-                        $scope.ok           = function ()                                                               {
-                            modelProducts.create({name: $scope.name, description: $scope.description}, function (err, id){
-                                console.log(err, id);
-                                modalInstance.close();
-                            });
-                        };
-
-                        $scope.cancel       = function ()                                                               {
-                            modalInstance.dismiss('cancel');
-                        };
-                    }]
+                    controller: "menuNewProductController"
                 });
             };
         }]
     }
 });
+
+angular.module('spaExercise').controller('menuNewProductController', ['$scope', '$uibModalInstance', 'modelProducts', function($scope, $uibModalInstance, modelProducts) {
+    $scope.name         = '';
+    $scope.description  = '';
+
+    $scope.ok           = function ()                                                                                   {
+        modelProducts.create({name: $scope.name, description: $scope.description}, function (err, id){
+            console.log(err, id);
+            $uibModalInstance.close();
+        });
+    };
+
+    $scope.cancel       = function ()                                                                                   {
+        $uibModalInstance.dismiss('cancel');
+    };
+}]);
